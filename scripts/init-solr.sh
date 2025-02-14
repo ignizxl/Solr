2010199eda15dadfa6661920ff7755025ba6e9b7
Natália S. Gomes <nataliandsg32@gmail.com>
2025-02-14 16:01:28 -0300

    Adiciona script init-solr.sh para criação do core e modificação do schema do Solr

---
+++ scripts/init-solr.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -e
+
+core_name="${CORE_NAME}"
+solr_url="${SOLR_URL}"
+
+echo "🕒 Aguardando o Solr iniciar..."
+solr start
+sleep 5
+echo "✅ Solr iniciado!"
+
+if curl -s "${solr_url}/admin/cores?action=STATUS" | grep -q "\"${core_name}\""; then
+    echo "🟢 Core '${core_name}' já existe."
+else
+    echo "⚙️ Criando core '${core_name}'..."
+    solr create -c "${core_name}"
+    echo "✅ Core '${core_name}' criado!"
+fi
+
+sleep 5
+
+# Adicionar campos ao schema após a criação do core
+echo "Adicionando campos ao schema do core '${core_name}'..."
+
+curl -X POST -H "Content-Type: application/json" \
+    --data '{
+      "add-field": {
+        "name": "codigo",
+        "type": "string",
+        "indexed": true,
+        "stored": true
+      }
+    }' \
+    "${solr_url}/${core_name}/schema"
+
+curl -X POST -H "Content-Type: application/json" \
+    --data '{
+      "add-field": {
+        "name": "titulo",
+        "type": "text_general",
+        "indexed": true,
+        "stored": true
+      }
+    }' \
+    "${solr_url}/${core_name}/schema"
+
+echo "Campos 'codigo' e 'titulo' adicionados com sucesso ao schema!"
+
+tail -f /opt/bitnami/solr/logs/solr.log
