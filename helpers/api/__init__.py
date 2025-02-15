da581857868d5a04456b891e57f0fec469086d37
João Igor <igor.joao@academico.ifpb.edu.br>
2025-02-14 23:52:21 -0300

    feat: adicionar novas rotas para testes

--- helpers/api/__init__.py
+++ helpers/api/__init__.py
@@ -1,14 +1,14 @@
 from flask_restful import Api
-from flask import Flask
+from resources.documentos import Documento, SolrSync
 
-# Importando os recursos
-from resources.documentos import Documento
-
-# Criando a aplicação Flask
-app = Flask(__name__)
-
-# Inicializando a API com o prefixo '/api'
-api = Api(app, prefix="/api")
+# Criando a instância da API com prefixo '/api'
+api = Api(prefix="/api")
 
 # Definindo as rotas da API
-api.add_resource(Documento, '/documentos', '/documentos/<int:documento_id>')
+api.add_resource(Documento, 
+    '/documentos', 
+    '/documentos/<int:documento_id>', 
+    '/documentos/exportar_json', 
+    '/documentos/enviar_para_solr'
+)
+api.add_resource(SolrSync, '/documentos/sync_solr')
