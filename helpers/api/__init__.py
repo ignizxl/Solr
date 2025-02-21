661b2a91e14bda3d16824fe2263a926cb0ad49e1
João Igor <igor.joao@academico.ifpb.edu.br>
2025-02-20 22:09:23 -0300

    cleanup: remover endpoints de teste

--- helpers/api/__init__.py
+++ helpers/api/__init__.py
@@ -1,5 +1,5 @@
 from flask_restful import Api
-from resources.documentos import Documento, SolrSync
+from resources.documentos import Documento
 
 # Criando a instância da API com prefixo '/api'
 api = Api(prefix="/api")
@@ -7,8 +7,4 @@ api = Api(prefix="/api")
 # Definindo as rotas da API
 api.add_resource(Documento, 
     '/documentos', 
-    '/documentos/<int:documento_id>', 
-    '/documentos/exportar_json', 
-    '/documentos/enviar_para_solr'
 )
-api.add_resource(SolrSync, '/documentos/sync_solr')
