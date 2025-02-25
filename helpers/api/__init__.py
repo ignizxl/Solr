f17b3ad11216cae463167df249fa7355dbc6bfd9
João Igor <igor.joao@academico.ifpb.edu.br>
2025-02-24 21:51:05 -0300

    feat: readicionar endpoints de teste

--- helpers/api/__init__.py
+++ helpers/api/__init__.py
@@ -1,5 +1,5 @@
 from flask_restful import Api
-from resources.documentos import Documento
+from resources.documentos import Documento, SolrSync
 
 # Criando a instância da API com prefixo '/api'
 api = Api(prefix="/api")
@@ -7,4 +7,8 @@ api = Api(prefix="/api")
 # Definindo as rotas da API
 api.add_resource(Documento, 
     '/documentos', 
+    '/documentos/<int:documento_id>', 
+    '/documentos/exportar_json', 
+    '/documentos/enviar_para_solr'
 )
+api.add_resource(SolrSync, '/documentos/sync_solr')
